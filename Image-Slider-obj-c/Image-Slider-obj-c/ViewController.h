b2f07c4dcefa838f08f1bba7b92c6e2a39709486
Yash G <98229201+yashgoyalg@users.noreply.github.com>
2023-05-09 12:26:50 +0530

    Add files via upload

---
+++ Image-Slider-obj-c/Image-Slider-obj-c/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Image-Slider-obj-c
+//
+//  Created by SwordMac32 on 04/05/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Image-Slider-obj-c/Image-Slider-obj-c/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Image-Slider-obj-c
+//
+//  Created by SwordMac32 on 04/05/23.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
